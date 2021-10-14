42785035e4a20819ec799aed35ae560f1ebaff7e
Graculus-Green <alex.b.blackburn@gmail.com>
2021-10-14 21:39:58 +0100

    When someone wins, stops inputs

--- tictactoe.js
+++ tictactoe.js
@@ -23,34 +23,9 @@ const init = (() => {
     return {board, restart}
 })();
 
-const turn = () => {  
-    if (currentPlayer.getplayerName() === "&#215;") {
-        return currentPlayer = O;
-    }
-    else {
-        return currentPlayer = X;
-    }  
-};
+
 
 //Modules
-
-/*const placeMarker = (() => {
-
-    let place = () =>{
-        document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', e => {
-            if (init.board[e.target.id] !== "") {
-                console.log("That cell isn't available, choose another");
-            }
-            else {
-            init.board[e.target.id] = `${currentPlayer.getplayerName()}`;
-            e.target.innerHTML = currentPlayer.getMark();
-            winGame.win();           
-            }
-        }));
-        
-    };
-    return {place};
-})();*/
 
 const winGame = ((player) => {
     let gameOn = true;
@@ -69,22 +44,35 @@ const winGame = ((player) => {
 
     const checkWin = () => {
         
-        winningPatterns.forEach((pattern) => {
-        let i = pattern[0], j = pattern[1], k = pattern[2];
-        if(board[i]!="" && board[i]==board[j] && board[j]==board[k]) {
-            
-            document.querySelector(".winner").innerHTML= `winner is: ${board[i]}`;
-            
-            return true;
+
+        for (let pattern = 0; pattern < winningPatterns.length; pattern++) {
+            let i = winningPatterns[pattern][0], j = winningPatterns[pattern][1], k = winningPatterns[pattern][2];
+            if(board[i]!="" && board[i]==board[j] && board[j]==board[k]) {
+                
+                return true;
+            };
+        };
+    };
+
+    const turn = () => {  
+        if (currentPlayer.getplayerName() === "&#215;") {
+            return currentPlayer = O;
         }
-      });
+        else {
+            return currentPlayer = X;
+        }  
     };
 
     const win = () => {
     //if any winningPatterns in playerMarkList => win
-        if (checkWin()=== true) {
+        if (checkWin()) {
+            //document.querySelector(".winner").innerHTML= `winner is: ${winMark}`;
+            init.board = ["_", `${currentPlayer.getplayerName()}`, "-","_", "I","S","W","I","N"]
+            return document.querySelector(".winner").innerHTML= `The winner is: ${currentPlayer.getplayerName()}`;
+
+
             console.log("WOOOOOOOO");
-            init.restart();
+            //init.restart();
         }
         //else if freeCell == true = > switchPlayer
         else if (board.includes("")) {
@@ -93,7 +81,7 @@ const winGame = ((player) => {
         }
         //else => draw
         else {
-            return "Draw"
+            return document.querySelector(".winner").innerHTML= `It's a draw.`;
         };
     };
 
@@ -109,15 +97,12 @@ const winGame = ((player) => {
             }
         }));  
     };
-return {place, win, gameOn};
+    return {place, gameOn};
 })();
 
-let makeAI = () => {
-
-};
+//let makeAI = () => {};
 
 // Game logic
-
 
 init;
 winGame.place(currentPlayer.getMark());
